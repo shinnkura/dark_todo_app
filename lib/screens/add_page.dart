05f941b691ac2bda4de4e51cdab0ba38a2a48035
shinnkura <n.shinzo1010@icloud.com>
2023-06-14 12:05:15 +0900

    [POST]Create Todo Task

--- lib/screens/add_page.dart
+++ lib/screens/add_page.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+import 'dart:html';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class AddTodoPage extends StatefulWidget {
   const AddTodoPage({super.key});
@@ -8,6 +12,9 @@ class AddTodoPage extends StatefulWidget {
 }
 
 class _AddTodoPageState extends State<AddTodoPage> {
+  TextEditingController titleController = TextEditingController();
+  TextEditingController descriptionController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,28 +22,74 @@ class _AddTodoPageState extends State<AddTodoPage> {
         title: const Text('Add Todo'),
       ),
       body: ListView(
-        children: const [
+        padding: const EdgeInsets.all(16),
+        children: [
           TextField(
-            decoration: InputDecoration(
-              hintText: 'Title',
-            ),
+            controller: titleController,
+            decoration: const InputDecoration(hintText: 'Title'),
           ),
           TextField(
-            decoration: InputDecoration(
+            controller: descriptionController,
+            decoration: const InputDecoration(
               hintText: 'Description',
             ),
             minLines: 3,
             maxLines: 5,
           ),
-          SizedBox(
+          const SizedBox(
             height: 16,
           ),
           ElevatedButton(
-            onPressed: null,
+            onPressed: submitData,
             child: Text('Add'),
           ),
         ],
       ),
     );
+  }
+
+  Future<void> submitData() async {
+    final title = titleController.text;
+    final description = descriptionController.text;
+    final body = {
+      "title": title,
+      "description": description,
+      "is_completed": false,
+    };
+    final url = 'http://api.nstack.in/v1/todos';
+    final uri = Uri.parse(url);
+    final response = await http.post(
+      uri,
+      body: jsonEncode(body),
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      },
+    );
+
+    if (response.statusCode == 201) {
+      titleController.text = '';
+      descriptionController.text = '';
+      print('Creation Success');
+      showSuccessMessage('Todo Created Successfully');
+    } else {
+      showErroMessage('Creation Error');
+    }
+  }
+
+  void showSuccessMessage(String message) {
+    final snackBar = SnackBar(content: Text(message));
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  }
+
+  void showErroMessage(String message) {
+    final snackBar = SnackBar(
+      content: Text(
+        message,
+        style: const TextStyle(color: Colors.white),
+      ),
+      backgroundColor: Colors.red,
+    );
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
   }
 }
